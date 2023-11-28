ca4c37e49f2d5aae467edeae70156667161e759f
Prasad <prasad.kolapkar94@gmail.com>
2023-11-28 13:14:37 +0530

    basic app setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div className="categories-container  ">
+      <div className="category-container">
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default App;
